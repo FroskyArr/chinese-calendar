15080db8b80962e3834545b03067548bc19d37da
Patrick 'FroskyArr' Chen <51911333+FroskyArr@users.noreply.github.com>
2021-08-15 04:38:27 +0800

    add datatype for calendar of a year

--- src/Data/Time/Calendar/Chinese.hs
+++ src/Data/Time/Calendar/Chinese.hs
@@ -19,3 +19,28 @@ fromDay = undefined
 
 toDay :: ChnDay -> Day
 toDay = undefined
+
+data C = C
+  { yearGregorian :: Int
+  , springFestival :: Day
+  , isLeapYear :: Bool
+  , leapMonth :: Maybe Int
+  , dayOfMonth :: [Bool]
+  , solarTermOffset :: [Int]
+  }
+
+
+
+
+{-
+
+1-4: leapMonth (0-12), 0 => Not a leap year
+5-17: MonthLength, 0 => shorter month (30 days), 1 => longer month (31 days)
+18-23: Spring Festival date offset to 01/01
+24: empty
+25-72: solar term offset
+
+9 bytes
+
+
+-}
