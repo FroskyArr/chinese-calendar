ba06dd553eb5f4545b382ba19b0bb8e122d2f5dd
Patrick 'FroskyArr' Chen <51911333+FroskyArr@users.noreply.github.com>
2021-08-17 10:51:24 +0800

    add test for decode

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,12 @@
+import Test.Hspec
+import Test.QuickCheck
+
+import Data.Time.Calendar.Chinese
+
+import qualified Data.ByteString.Lazy as BSL
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = print $ decode year2020
+
+year2020 :: BSL.ByteString
+year2020 = BSL.pack [0xE4, 0x52, 0x55, 0xB0, 0x69, 0, 0, 0, 0x4]
