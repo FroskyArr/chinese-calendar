0116a1bf1bee4b45544a24623afe1a83f149a471
Patrick 'FroskyArr' Chen <51911333+FroskyArr@users.noreply.github.com>
2021-08-17 22:25:48 +0800

    use hspec in test

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,18 @@ import Data.Time.Calendar.Chinese
 import qualified Data.ByteString.Lazy as BSL
 
 main :: IO ()
-main = print $ decode year2020
+main = hspec $ do
+  describe "decode" $
+    it "" $
+      decode year2020 `shouldBe` year2020'
 
 year2020 :: BSL.ByteString
 year2020 = BSL.pack [0x47, 0x4A, 0xB0, 0x45, 0, 0, 0, 0, 0x4]
+
+year2020' :: C
+year2020' = C
+  { springFestival = 24
+  , leapMonth = Just 4
+  , dayOfMonth = [False,True,True,True,False,True,False,False,True,False,True,False,True]
+  , solarTermOffset = [1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
+  }
